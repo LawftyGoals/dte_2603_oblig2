aa887c700d15ad618685de29aba26dc6dcbcb04c
Lawfty Goals <mymsnadressis@hotmail.com>
2025-02-28 14:14:27 +0100

    fix an error

--- app/src/main/java/com/example/dte2603_oblig2/MainActivity.kt
+++ app/src/main/java/com/example/dte2603_oblig2/MainActivity.kt
@@ -5,13 +5,38 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults.elevatedCardElevation
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,13 +44,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.dte2603_oblig2.ui.theme.Dte2603_oblig2Theme
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.KeyboardArrowDown
-import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.CardDefaults.elevatedCardElevation
-import androidx.compose.foundation.clickable
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,9 +66,15 @@ fun MainScreen() {
     var checklists by remember {
         mutableStateOf(
             listOf(
-                Checklist("Min todo-liste", listOf("Skriv søknad", "Send søknad", "Få jobb", "Jobb hardt")),
+                Checklist(
+                    "Min todo-liste",
+                    listOf("Skriv søknad", "Send søknad", "Få jobb", "Jobb hardt")
+                ),
                 Checklist("Husvask", listOf("Kjøkkenet", "Badet", "Stua", "Soverom")),
-                Checklist("Studieplan", listOf("Gjør matteoppgaver", "Gjør fysikkoppgaver", "Gjør kjemioppgaver"))
+                Checklist(
+                    "Studieplan",
+                    listOf("Gjør matteoppgaver", "Gjør fysikkoppgaver", "Gjør kjemioppgaver")
+                )
             )
         )
     }
@@ -59,7 +83,7 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         topBar = {
             // A simple top app bar
-            SmallTopAppBar(
+            TopAppBar(
                 title = { Text("MineHuskelister") },
                 actions = {
                     // Toggle between 2 columns and 1 column
