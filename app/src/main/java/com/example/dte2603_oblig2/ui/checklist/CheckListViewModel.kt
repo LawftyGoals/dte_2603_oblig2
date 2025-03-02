e65643d893cedeb524cce7202e5c593d903b0c78
Lawfty Goals <mymsnadressis@hotmail.com>
2025-03-02 13:39:11 +0100

    remove unused functions

--- app/src/main/java/com/example/dte2603_oblig2/ui/checklist/CheckListViewModel.kt
+++ app/src/main/java/com/example/dte2603_oblig2/ui/checklist/CheckListViewModel.kt
@@ -3,7 +3,6 @@ package com.example.dte2603_oblig2.ui.checklist
 import androidx.lifecycle.ViewModel
 import com.example.dte2603_oblig2.data.CheckList
 import com.example.dte2603_oblig2.data.CheckListItem
-import com.example.dte2603_oblig2.data.DTOCheckList
 import com.example.dte2603_oblig2.data.DTOCheckListItem
 import com.example.dte2603_oblig2.data.UiState
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -21,30 +20,6 @@ class CheckListViewModel : ViewModel() {
         _uiState.update { currentState -> currentState.copy(isMultiColumn = !currentState.isMultiColumn) }
     }
 
-    fun updateCheckList(prevCheckList: CheckList, updatedCheckList: CheckList) {
-        _uiState.update { currentState ->
-            val updatedCheckLists = currentState.checkLists.map {
-                if (it.checkListId ==
-                    prevCheckList
-                        .checkListId
-                ) {
-                    CheckList(
-                        it.checkListId, updatedCheckList.name, updatedCheckList.icon,
-                        updatedCheckList
-                            .checkListItems
-                    )
-                } else {
-                    it
-                }
-            }
-
-            currentState.copy(
-                checkLists = updatedCheckLists.toMutableList(),
-                checkListCount = updatedCheckLists.count()
-            )
-        }
-    }
-
     fun deleteCheckList(checkList: CheckList) {
         _uiState.update { currentState ->
             val tempCheckLists = currentState.checkLists
@@ -52,24 +27,6 @@ class CheckListViewModel : ViewModel() {
             currentState.copy(
                 checkLists = tempCheckLists,
                 checkListCount = tempCheckLists.count()
-
-            )
-        }
-    }
-
-    fun addCheckList(checkList: DTOCheckList) {
-        _uiState.update { currentState ->
-            val tempCheckLists = currentState.checkLists
-            tempCheckLists.add(
-                CheckList(
-                    currentState.checkListIdValue, checkList.name, checkList
-                        .icon, checkList.checkListItems
-                )
-            )
-            currentState.copy(
-                checkLists = tempCheckLists,
-                checkListCount = tempCheckLists.count(),
-                checkListIdValue = currentState.checkListItemIdValue + 1
 
             )
         }
@@ -103,41 +60,6 @@ class CheckListViewModel : ViewModel() {
                 }
             }
             currentState.copy(checkLists = tempCheckLists.toMutableList())
-        }
-    }
-
-    fun addCheckListItem(checkList: CheckList, checkListItem: DTOCheckListItem) {
-        _uiState.update { currentState ->
-            val tempList = currentState.checkLists
-            val targetCheckList = tempList.firstOrNull { it.checkListId == checkList.checkListId }
-
-            targetCheckList?.checkListItems?.add(
-                CheckListItem(
-                    currentState.checkListItemIdValue,
-                    checkListItem.name, checkListItem.checked
-                )
-            )
-
-            currentState.copy(
-                checkLists = tempList,
-                checkListItemCount = currentState.checkListItemCount + 1,
-                checkListItemIdValue = currentState.checkListIdValue + 1,
-            )
-        }
-    }
-
-    fun deleteCheckListItem(checkList: CheckList, checkListItem: CheckListItem) {
-        _uiState.update { currentState ->
-
-            val tempList = currentState.checkLists
-            val targetCheckList = tempList.firstOrNull { it == checkList }
-
-            targetCheckList?.checkListItems?.remove(checkListItem)
-
-            currentState.copy(
-                checkLists = tempList,
-                checkListItemCount = currentState.checkListItemCount - 1
-            )
         }
     }
 
